32490495ace9bde08c4ff45abee58611d0ee1677
Xpyst <qjq3@mail.ru>
2024-03-19 20:14:04 +0300

    corrected view component title

--- resources/views/components/title.blade.php
+++ resources/views/components/title.blade.php
@@ -2,27 +2,22 @@
 
 <div class="border-bottom mb-3">
 
-    @isset($link)
+    <div class="d-flex justify-content-between align-items-center mb-3">
+
+        @isset($link)
 
         <div class="mb-2">
             {{ $link }}
         </div>
 
-    @endisset
-
-    <div class="d-flex justify-content-between align-items-center">
+        @endisset
 
         {{ $slot }}
 
         @isset($right)
-
-            <div>
-
-                {{ $right }}
-
-            </div>
-
+            {{ $right }}
         @endisset
+
     </div>
 
 </div>
--- resources/views/user/posts/index.blade.php
+++ resources/views/user/posts/index.blade.php
@@ -5,16 +5,12 @@
 
 @section('main.content')
 
-
     <x-title>
-
-        <h1 class="h2 mb-5">
-
+        <h1 class="h2">
             {{ __('Мои Посты') }}
-
         </h1>
 
-        <x-slot  name="right">
+        <x-slot name="right">
 
             <x-button-link href=" {{route('user.posts.create')}}" >
 
@@ -23,9 +19,8 @@
             </x-button-link>
 
         </x-slot>
-
     </x-title>
-   
+
 
     @if(empty($posts))
 
@@ -38,30 +33,30 @@
             <x-card padding='p-2'>
                 <x-card-body>
                     <div class="mb-3">
-    
-                        <a href="{{ route('user.posts.show', $post->id) }}"> 
+
+                        <a href="{{ route('user.posts.show', $post->id) }}">
                             <h2 class="h6 m-0">
                                 {{ $post->title }}
                             </h2>
                         </a>
-    
+
                         <div class="small text-muted mb-0 mt-0">
-    
+
                             {{ $post->published_at?->format('d.m.Y H:i:s') }}
-                    
+
                         </div>
-    
+
                     </div>
                 </x-card-body>
             </x-card>
-    
+
         @endforeach
 
         {{ $posts->links() }}
-        
-    @endif
-    
 
-    
-  
+    @endif
+
+
+
+
 @endsection
