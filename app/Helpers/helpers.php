41972382b5e20ea149fd844d4f93e5e7b6d1357a
Xpyst <qjq3@mail.ru>
2024-11-01 03:10:21 +0300

    работа над css header + ссылка

--- app/Helpers/helpers.php
+++ app/Helpers/helpers.php
@@ -4,10 +4,8 @@ use Illuminate\Support\Facades\Route;
 
 if( ! function_exists('active_link') ){
 
-    function active_link(string $name, string $classNotActive = 'active-link'): string
+    function active_link(string $name, string $classActive = 'active', string $classNotActive = ''): string
     {
-        $classNotActive = "block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700";
-        $classActive = "block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500";
         return Route::is($name) ? $classActive : $classNotActive;
     }
 
--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -1,52 +1,45 @@
-<header class="dark:bg-gray-700 h-custom-height">
+<header class=" dark:bg-gray-700 h-custom-height">
 
-    <nav class="backdrop-blur-md h-custom-height dark:bg-dark-gray-opacity text-white shadow-md fixed w-full z-20 top-0 start-0">
-        <div data-collapse-navmenu="div-menu" class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-
+    <nav class="backdrop-blur-sm h-custom-height dark:bg-dark-gray-opacity text-white shadow-md fixed w-full top-0 start-0">
+        <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
             <a href="https://flowbite.com/" class="flex items-center space-x-3 rtl:space-x-reverse">
                 <img src="https://flowbite.com/docs/images/logo.svg" class="h-8" alt="Flowbite Logo">
                 <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span>
             </a>
-
-            <div data-collapse-navmenu="menu" class="flex items-center justify-between w-full md:flex md:w-auto order-1">
-                <ul class="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0">
+            <div class="flex lg:order-2 gap-3">
+                <a href="{{ route('register') }}" class="mr-2 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Регистрация</a>
+                <a href="" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Вход</a>
+                <button data-collapse-toggle="navbar-sticky" type="button" class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg lg:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-sticky" aria-expanded="false">
+                    <span class="sr-only">Open main menu</span>
+                    <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
+                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15"/>
+                    </svg>
+                </button>
+            </div>
+            <div class=" items-center justify-between hidden w-full lg:flex lg:w-auto lg:order-1" id="navbar-sticky">
+                <ul class="flex flex-col p-4 lg:p-0 mt-5 max-lg:hover:text-blue-700 max-lg:dark:bg-dark-black-opacity font-medium border-gray-100 rounded-lg lg:space-x-8 rtl:space-x-reverse lg:flex-row lg:mt-0 lg:border-0">
                     <li>
-                        <a href="{{ route('home') }}" class="{{ active_link('home') }}" aria-current="page">
+                        {{-- {{ active_link('home') }}  --}}
+                        <a href="{{ route('home') }}" class=" {{ active_link('home', 'bg-gray-700') }} block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 max-lg:hover:bg-transparent max-lg:hover:text-blue-700 max-lg:p-0 max-lg:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white max-lg:dark:hover:bg-transparent dark:border-gray-700" aria-current="page">
                             {{ __('Главная') }}
                         </a>
                     </li>
                     <li>
-                        <a href="#" class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Блог Разработчиков</a>
+                        <a href="#" class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 max-lg:hover:bg-transparent max-lg:hover:text-blue-700 max-lg:p-0 max-lg:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white max-lg:dark:hover:bg-transparent dark:border-gray-700">Блог Разработчиков</a>
                     </li>
                     <li>
-                        <a href="#" class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Блог Дизайнеров</a>
+                        <a href="#" class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 max-lg:hover:bg-transparent max-lg:hover:text-blue-700 max-lg:p-0 max-lg:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white max-lg:dark:hover:bg-transparent dark:border-gray-700">Блог Дизайнеров</a>
                     </li>
                     <li>
-                        <a href="#" class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">О нас</a>
+                        <a href="#" class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 max-lg:hover:bg-transparent max-lg:hover:text-blue-700 max-lg:p-0 max-lg:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white max-lg:dark:hover:bg-transparent dark:border-gray-700">О нас</a>
                     </li>
                     <li>
-                        <a href="#" class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Контакты</a>
+                        <a href="#" class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 max-lg:hover:bg-transparent max-lg:hover:text-blue-700 max-lg:p-0 max-lg:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white max-lg:dark:hover:bg-transparent dark:border-gray-700">Контакты</a>
                     </li>
                 </ul>
             </div>
-
-            <div class="order-2 flex space-x-3 rtl:space-x-reverse">
-                <a href="{{ route('register') }}" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Регистрация</a>
-                <a href="{{ route('login') }}" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4
-                focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4
-                py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Вход</a>
-                <div data-collapse-navmenu="buttonBurger__wrapp">
-                    <button data-collapse-navmenu="buttonBurger" type="button" class="order-3 inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg lg:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-sticky" aria-expanded="true">
-                        <span class="sr-only">Открыть главное меню</span>
-                        <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
-                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15"/>
-                        </svg>
-                    </button>
-                </div>
-            </div>
-
-
         </div>
     </nav>
 
 </header>
+
