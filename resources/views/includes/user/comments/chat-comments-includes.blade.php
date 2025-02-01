3d820b19c04abab18bd1bcf93f4af6b72b0ce04f
Xpyst <qjq3@mail.ru>
2025-02-01 19:38:47 +0300

    убран blade комменатрий

--- resources/views/includes/user/comments/chat-comments-includes.blade.php
+++ resources/views/includes/user/comments/chat-comments-includes.blade.php
@@ -4,20 +4,6 @@
         <span class="text-white text-2md">Комментарии</span>
     </div >
 
-
-    {{-- @foreach ($post->comments as $comment)
-
-        @if($accum % 2)
-            @php $accum++ @endphp
-            <x-comments.card-comment post="$post" dropdownDotsNumber="1" />
-        @else
-            @php $accum++ @endphp
-            <x-comments.card-comment orientation="right" post="$post" dropdownDotsNumber="2"/>
-        @endif
-
-    @endforeach --}}
-
-
     @foreach ($post->comments as $comment)
         <x-comments.card-comment
             :post="$post"
@@ -25,8 +11,5 @@
             :orientation="$loop->even ? 'right' : 'left' "
         />
     @endforeach
-
-
-
 
 </div>
