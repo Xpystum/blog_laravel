9ce6da95cb077dc7beb8244c98ddc3da7a437033
Xpyst <qjq3@mail.ru>
2025-02-10 00:31:12 +0300

    добавлены комменатрии в AppServiceProvide

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -20,26 +20,29 @@ class AppServiceProvider extends ServiceProvider
         $this->app->bind(IUserRepository::class,  UserRepository::class);
     }
 
-    /**
-     * Bootstrap any application services.
-     */
     public function boot(): void
     {
-        //расшарить переменную на все шаблоны блейда
+        {
+            #TODO Указано тестово потом убрать
+
+               //расшарить переменную на все шаблоны блейда
         View::share('date', date('Y'));
 
         //расшарить переменную только на маршруты начинающийся с user
         View::composer('user*', function($view){
             $view->with('balance', 12345);
         });
+        }
 
 
-        Paginator::useBootstrapFive();
 
         //указываем тестовые маршруты для запусков в тестах p.s тесты не воспринимают наши routes
         $this->mapTestingRoutes();
     }
 
+    /**
+    * Регистрируем наши маршруты для тестов
+    */
     protected function mapTestingRoutes()
     {
         if (! App::environment('testing')) {
