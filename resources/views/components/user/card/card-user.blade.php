f1d0aae0b8e23a0f0fd81a26c82c6f1861042958
Xpyst <qjq3@mail.ru>
2025-06-03 17:29:33 +0300

    продолжения верстки чата

--- resources/views/components/user/card/card-user.blade.php
+++ resources/views/components/user/card/card-user.blade.php
@@ -1,8 +1,10 @@
 @props(['src', 'user', 'alt' => 'Изображения', 'span_name' => ''])
 
 <div {{ $attributes->class($attributes->get('class', 'flex flex-row')) }}>
-    <a href="{{ route('users.profiles', $user->id) }}" class="w-10 h-10 md:w-12 md:h-12 p-0.5 rounded-full ring-gray-300 dark:ring-gray-500 overflow-hidden">
-        <img class="rounded-full ring-1 ring-gray-300 dark:ring-gray-500"  src='{{ $src }}' alt='{{ $alt }}' />
+    <a href="{{ route('users.profiles', $user->id) }}" class="">
+        <img class="w-12 min-w-12 max-w-12 h-12 rounded-full flex-shrink-0"
+                src="{{ asset($user->profile->url_avatar) }}" alt="{{ $user->login }}">
+        {{-- <img class="rounded-full ring-1 ring-gray-300 dark:ring-gray-500"  /> --}}
     </a>
     <a href="{{ route('users.profiles', $user->id) }}" class="flex items-center text-white dark:text-white text-center pl-2 hover:underline">
         {{ $span_name }}
