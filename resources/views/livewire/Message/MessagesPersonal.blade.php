430fd4e198960e9bd0acb2a9fe04eb9885a123c3
Xpyst <qjq3@mail.ru>
2025-06-05 23:55:39 +0300

    сделать замораживание кнопки

--- resources/views/livewire/Message/MessagesPersonal.blade.php
+++ resources/views/livewire/Message/MessagesPersonal.blade.php
@@ -29,7 +29,11 @@
                     placeholder="Write a comment..." required></textarea>
             </div>
             <div class="flex items-center justify-between px-3 py-2 border-t dark:border-gray-600 border-gray-200">
-                <button wire:click="sendMessage" type="submit"
+                <button
+                    wire:click="sendMessage"
+                    wire:loading.attr="disabled"
+                    wire:loading.class="opacity-50 cursor-not-allowed"
+                    type="submit"
                     class="inline-flex items-center py-2.5 px-4 text-xs font-medium text-center text-white bg-blue-700 rounded-lg focus:ring-4 focus:ring-blue-200 dark:focus:ring-blue-900 hover:bg-blue-800">
                     Post comment
                 </button>
